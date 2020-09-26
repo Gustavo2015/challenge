67c7399d39604a807da738333866f313b156547d
Gustavo <gustavo.vieira@dubbox.com.br>
2020-09-26 15:38:16 -0300

    gh-pages

---
+++ precache-manifest.3019ad7bdbd93da8187e0221747728bb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c806b826483bcbcfcd745d7fcf5dd5a6",
+    "url": "/challenge/.htaccess"
+  },
+  {
+    "revision": "78d83817c6d3bbc60613",
+    "url": "/challenge/css/about.74db159b.css"
+  },
+  {
+    "revision": "b0f54439af3e18390497",
+    "url": "/challenge/css/app.5937bc67.css"
+  },
+  {
+    "revision": "212eab771ad3f9cbe820",
+    "url": "/challenge/css/chunk-vendors.6a7c8892.css"
+  },
+  {
+    "revision": "8c49e37c7ffb9f267f6ffd290ed5719d",
+    "url": "/challenge/favicon.png"
+  },
+  {
+    "revision": "50b2b836ea4dc4255e1c806900d4aa98",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "78d83817c6d3bbc60613",
+    "url": "/challenge/js/about.d4a03a0a.js"
+  },
+  {
+    "revision": "b0f54439af3e18390497",
+    "url": "/challenge/js/app.7201f719.js"
+  },
+  {
+    "revision": "212eab771ad3f9cbe820",
+    "url": "/challenge/js/chunk-vendors.b488d004.js"
+  },
+  {
+    "revision": "e6969ac3dfa7f74756c0e73161a9822c",
+    "url": "/challenge/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/challenge/robots.txt"
+  }
+]);
